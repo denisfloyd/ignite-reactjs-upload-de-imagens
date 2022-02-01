945b6e2fab77adcb7796406d4860ea7be2f511b2
denismend <denisladeira1@gmail.com>
2022-02-01 19:08:38 -0300

    feat(home): :sparkles: index page
    get images and treat data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
+import Head from 'next/head';
+
 import { useInfiniteQuery } from 'react-query';
+
+import { Button, Box } from '@chakra-ui/react';
 
 import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
@@ -8,36 +11,74 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type IInfiniteQueryResponse = {
+  after: number | null;
+  data: Card[];
+};
+
 export default function Home(): JSX.Element {
+  const getDBImages = async ({
+    pageParam = null,
+  }): Promise<IInfiniteQueryResponse> => {
+    const { data } = await api.get('/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  };
+
   const {
     data,
     isLoading,
     isError,
     isFetchingNextPage,
     fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery(
+    hasNextPage = null,
+  } = useInfiniteQuery<unknown, unknown, IInfiniteQueryResponse>(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    getDBImages,
+    {
+      getNextPageParam: (lastPage: { after: number }) => lastPage.after,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const imgsData = data?.pages.map(page => page.data).flat();
+    return imgsData;
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
   // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
+      <Head>
+        <title>UpFi</title>
+        <link rel="shortcut icon" href="logo.svg" />
+      </Head>
+
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt={8} onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
